95a80c4f3b850b13e60f97b2f2765795d6cbef4f
Natalia <atvnataliajn@gmail.com>
2022-03-06 17:54:50 +0100

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,19 +1,32 @@
 'use strict';
 
 //          GLOBAL CONSTS         //
+// homepage
+const homepageModal = document.querySelector('.js-homepageModal');
+const startBtn = document.querySelector('.js-startBtn');
+
+// transition homepage to gamepage
+const countDownParagraph = document.querySelector('.js-countDown');
+
+// gamepage
 const scoreParagraph = document.querySelector('.js-score');
-const timeLeft = document.querySelector('.js-timeLeft');
+//const timeLeft = document.querySelector('.js-timeLeft');
 const playBtn = document.querySelector('.js-playBtn');
-const stopBtn = document.querySelector('.js-stopBtn');
+const pauseBtn = document.querySelector('.js-pauseBtn');
 
 const holes = document.querySelectorAll('.js-hole');
 const moles = document.querySelectorAll('.js-mole');
 
 const cursor = document.querySelector('.js-cursor');
 
-let lastHole;
-let timeUp = false;
+let timeLeft;
 let score = 0;
+
+// let lastHole;
+// let highScore = 0;
+
+// game over modal
+const modal = document.querySelector('.js-modal');
 
 //          FUNCTIONS            //
 
@@ -33,6 +46,7 @@ const getRandomHole = () => {
   let holeIndex = getRandomNumber(holes.length);
   hole = holes[holeIndex];
   //return hole? para poder sacar lo demás a otra función
+
   // Appear the mole
   const holeContainer = hole.parentNode;
   const moleContainer = holeContainer.childNodes[3];
@@ -40,18 +54,51 @@ const getRandomHole = () => {
 
 };
 
-const startGame = () =>{
+// run game
+const runGame = () =>{
   playBtn.classList.add('hidden');
-  stopBtn.classList.remove('hidden');
+  pauseBtn.classList.remove('hidden');
   setInterval(getRandomHole, 700);
 };
 //setTimeout
 
-playBtn.addEventListener('click', startGame());
-stopBtn.addEventListener('click', () => {
-  clearInterval(startGame);
+
+// start game
+// countdown
+let countdown;
+const countdownToPlay = (sec) => {
+  countdown = sec;
+  const countdownDiv = document.createElement('div');
+
+  if(countdown > 0){
+    countdown--;
+    countdownDiv.appendChild(countdown);
+  } else{
+    countdownDiv.classList.add('hidden');
+  }
+};
+
+setInterval(countdownToPlay, 2000);
+
+
+//       LISTENERS       //
+// Start game
+startBtn.addEventListener('click', () =>{
+  homepageModal.classList.add('modalClosed');
+  countdownToPlay(3);
+  timeLeft= 20;
+  score= 0;
+  runGame();
+});
+
+// Run game
+playBtn.addEventListener('click', runGame());
+
+// Pause game
+pauseBtn.addEventListener('click', () => {
+  clearInterval(runGame);
   playBtn.classList.remove('hidden');
-  stopBtn.classList.add('hidden');
+  pauseBtn.classList.add('hidden');
 });
 
 // Move cursor
